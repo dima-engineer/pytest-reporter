e58f7857a25c05b8c1513e158b3ffc0ed4f92edc
Dmitry Marchuk <dmarchuk@remedypointsolutions.com>
2022-01-29 10:21:34 +0300

    Add input variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,28 @@
 #!/bin/bash -x
 
-python --version
+# $1: package-manager
+# $2: requirements file if pip is a package-manager
+# $4: pytest-root-dir
+# $5: tests dir
+# $6: cov-omit-list
+# $7: cov-threshold-single
+# $8: cov-threshold-total
+
+PACKAGE_MANAGER=${1:-"pip"}
+
+# Case insensitive comparing
+case ${PACKAGE_MANAGER,,} in
+"poetry")
+  python -m pip install 'poetry==1.1.11'
+  python -m poetry config virtualenvs.create false
+  python -m poetry install
+  python -m poetry add pytest pytest-cov pytest-mock
+  python -m poetry shell
+  ;;
+"pip")
+  if [ -f "$2" ]
+  then
+    python -m pip install -r "$2" --no-cache-dir --user
+  fi
+  python -m pip install pytest pytest-cov
+esac
